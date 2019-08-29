685cc7db2116da72118f7a31661f62f55ada207e
fillelvis42 <50189423+fillelvis42@users.noreply.github.com>
2019-08-28 23:16:58 -0500

    Add file via upload
    Testing upload

---
+++ Hello World.py
@@ -0,0 +1,8 @@
+#This is my first program
+c = input("Enter first_name: ")
+d = input("Enter last_name: ")
+a = float(input("Enter first_num: "))
+b = float(input("Enter last_num: "))
+#sum of the two values provided by the user
+e = a + b
+print("Hello " + c + " " + d + " Your lucky number is " + str(e))
